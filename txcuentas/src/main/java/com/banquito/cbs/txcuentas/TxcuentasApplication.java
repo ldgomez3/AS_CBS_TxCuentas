1fbb0a55e5c53b534f934fc1b5a87c42a9c3d68c
ldgomez3 <ldgomez3@espe.edu.ec>
2025-02-17 00:18:39 -0500

    Creación de proyecto

---
+++ txcuentas/src/main/java/com/banquito/cbs/txcuentas/TxcuentasApplication.java
@@ -0,0 +1,13 @@
+package com.banquito.cbs.txcuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxcuentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxcuentasApplication.class, args);
+	}
+
+}
