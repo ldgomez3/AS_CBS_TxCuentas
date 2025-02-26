8070c279409c4b509e14cb42a58b49e4e4abefaf
ldgomez3 <ldgomez3@espe.edu.ec>
2025-02-26 02:12:09 -0500

    DG - Crear repositorio para acceso a datos de transacciones

--- src/main/java/com/banquito/cbs/txcuentas/repositorio/TxCuentaRepository.java
+++ src/main/java/com/banquito/cbs/txcuentas/repositorio/TxCuentaRepository.java
@@ -1,8 +1,15 @@
 package com.banquito.cbs.txcuentas.repositorio;
 
-import com.banquito.cbs.txcuentas.modelo.Txcuentas;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.OffsetDateTime;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.banquito.cbs.txcuentas.modelo.Txcuentas;
+
+@Repository
 public interface TxCuentaRepository extends JpaRepository<Txcuentas, Integer> {
-    
+    List<Txcuentas> findByIdCuenta(Integer idCuenta);
+    List<Txcuentas> findByFechaHoraBetween(OffsetDateTime fechaInicio, OffsetDateTime fechaFin);
 }
